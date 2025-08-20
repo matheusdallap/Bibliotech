2c16c665c1d4b576e9cf2a0bd2f9df7b274046ce
dalla <matheusdallaths@gmail.com>
2025-08-20 00:28:43 -0300

    chore: Adição das URLs para os campos de create-user e login.

--- mysite/Bibliotech/urls.py
+++ mysite/Bibliotech/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create-user/', views.CreateUserView.as_view(), name='create_user'),
+    path('login/', views.LoginUserView.as_view(), name='login_user'),
 ]
