37a6266788d035d321c03b0e1814251d722c4f03
dalla <matheusdallaths@gmail.com>
2025-08-19 22:39:33 -0300

    feat: Implementação de código.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Bibliotech.apps.BibliotechConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
